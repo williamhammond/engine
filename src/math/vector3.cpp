f4579d4510f377f9199c38e6e722d7184f8f6714
William Hammond <william.t.hammond@gmail.com>
2022-06-23 09:29:59 -0400

    Adds division, normalization, multiplication and negation to vector3

--- src/math/vector3.cpp
+++ src/math/vector3.cpp
@@ -1,7 +1,5 @@
 #include "vector3.h"
 
-#include "utils.h"
-
 Vector3::Vector3(float x_in, float y_in, float z_in) {
   x = x_in;
   y = y_in;
@@ -11,7 +9,3 @@ Vector3::Vector3(float x_in, float y_in, float z_in) {
 float& Vector3::operator[](int i) { return ((&x)[i]); }
 
 const float& Vector3::operator[](int i) const { return ((&x)[i]); }
-
-float Vector3::magnitude() const {
-  return Utils::sqrt(Utils::square(x) + Utils::square(y) + Utils::square(z));
-}
--- src/math/vector3.h
+++ src/math/vector3.h
@@ -1,16 +1,43 @@
 #ifndef ENGINE_VECTOR3_H
 #define ENGINE_VECTOR3_H
 
+#include "utils.h"
+
 class Vector3 {
  private:
+ public:
   float x, y, z;
 
- public:
   Vector3() = default;
   Vector3(float x_in, float y_in, float z_in);
+
   float& operator[](int i);
   const float& operator[](int i) const;
-  float magnitude() const;
+
+  inline Vector3& operator*=(float a) {
+    x *= a;
+    y *= a;
+    z *= a;
+    return (*this);
+  }
+  inline Vector3& operator/=(float a) {
+    x /= a;
+    y /= a;
+    z /= a;
+    return (*this);
+  }
+  inline Vector3 operator*(float a) const { return {x * a, y * a, z * a}; }
+  inline Vector3 operator/(float a) const { return {x / a, y / a, z / a}; }
+  inline Vector3 operator-() const { return {-x, -y, -z}; }
+
+  [[nodiscard]] inline float Magnitude() const {
+    return Utils::sqrt(Utils::square(x) + Utils::square(y) + Utils::square(z));
+  }
+
+  inline Vector3 Normalize() {
+    // TODO handle 0 division properly
+    return ((*this) / (Magnitude() + 0.000000001f));
+  }
 };
 
 #endif  // ENGINE_VECTOR3_H
