faeaf5167f4f6497dde5468ccc85a0213804aa8f
William Hammond <william.t.hammond@gmail.com>
2022-07-26 12:33:44 -0400

    Makes builds work with numbers include

--- scripts/install-ubuntu-latest-gcc-deps.sh
+++ scripts/install-ubuntu-latest-gcc-deps.sh
@@ -1,7 +1,15 @@
 #/bin/sh
 
+sudo update-alternatives --remove-all gcc
+sudo update-alternatives --remove-all g++
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install ninja-build cmake
+sudo apt-get install ninja-build cmake gcc-10 g++-10
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 50
+
 
 ninja --version
 cmake --version
